9552cada834c9c30b8780d380ce4322b67f729f3
Krishna-Arjun <111630346+krishna-arjun-praximax@users.noreply.github.com>
2022-11-28 20:11:57 +0530

    dynamic_coloring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`flex w-full h-screen overflow-hidden ${
+        "bg-" + color + "-300"
+      }`}
+    >
+      <div className="flex flex-col w-full">
+        <div className="flex w-full mt-5 ml-10 space-x-3 justify-between font-semibold">
+          <div className="flex space-x-3">
+            <h1>ARTICLES</h1>
+            <h1>CONTRIBUTORS</h1>
+            <h1>LOGIN</h1>
+            <h1>SIGN UP</h1>
+          </div>
+          <div className="flex pr-16 space-x-3">
+            <button
+              onClick={() => setColor("red")}
+              className="w-8 h-8 rounded-full outline outline-2 bg-red-300"
+            ></button>
+            <button
+              onClick={() => setColor("cyan")}
+              className="w-8 h-8 rounded-full outline outline-2 bg-cyan-300"
+            ></button>
+            <button
+              onClick={() => setColor("violet")}
+              className="w-8 h-8 rounded-full outline outline-2 bg-violet-400"
+            ></button>
+          </div>
+        </div>
+
+        <div className="flex flex-col h-full items-center justify-center">
+          <p className="mb-5 font-semibold text-lg opacity-80">
+            You can get all type of article from one source.
+          </p>
+          <div className="flex w-96 h-24 items-center justify-center border rounded-lg">
+            <div className="flex space-x-3">
+              <input
+                type="text"
+                className="border outline-none rounded lg px-3 py-2"
+                placeholder="Enter article name"
+              />
+              <button className=" border px-2 rounded-lg bg-blue-600 text-white">
+                Search
+              </button>
+            </div>
+          </div>
+          <div className="flex mt-24 space-x-10">
+            <img className="w-52 h-52" src={require("./book1.jpg")} alt="" />
+            <img className="w-52 h-52" src={require("./cycle.jpg")} alt="" />
+            <img className="w-52 h-52" src={require("./science.jpg")} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: ["bg-red-300", "bg-cyan-300", "bg-violet-300"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
